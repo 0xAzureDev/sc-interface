1e555c6e2169fb863136ea1a6628d76f8c5981b5
AzureDev0 <azuredev0@gmail.com>
2022-08-20 20:33:48 +0100

    feat: new data types for abi

--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,27 @@ declare global {
   }
 }
 
+export type Abi = {
+  constant: boolean;
+  inputs: {
+    name: string;
+    type: string;
+  }[];
+  name: string;
+  outputs: {
+    name: string;
+    type: string;
+  }[];
+  payable: boolean;
+  stateMutability: string;
+  type: string;
+};
+
+export type AbiInputs = Abi["inputs"];
+export type AbiOutputs = Abi["outputs"];
+
+export type AbiList = Abi[];
+
 export type AbiObject = {
   payable: string;
   stateMutability: string;
