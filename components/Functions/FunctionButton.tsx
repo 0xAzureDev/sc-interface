77512a2cc928c6607d2994d9caec2bf8b76cd4e1
AzureDev0 <azuredev0@gmail.com>
2022-08-22 16:21:11 +0100

    feat: no wallet connected

--- components/Functions/FunctionButton.tsx
+++ components/Functions/FunctionButton.tsx
@@ -1,32 +1,44 @@
-import { METAMASK_DOWNLOAD_LINK } from "constants/";
-import { isInstalled } from "helpers";
-import { FC } from "react";
-import { FunctionButtonInterface } from "types";
+import { METAMASK_DOWNLOAD_LINK } from 'constants/'
+import { isInstalled } from 'helpers'
+import { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { walletAddress } from 'store/contractSlice'
+import { FunctionButtonInterface } from 'types'
 
 const FunctionButton: FC<FunctionButtonInterface> = ({ submitTx }) => {
+  const address = useSelector(walletAddress)
+
   return (
     <>
-      {isInstalled() ? (
+      {isInstalled() && address ? (
         <button
           className="header-button function"
           type="button"
-          style={{ marginBottom: "1rem", color: "#fff" }}
+          style={{ marginBottom: '1rem', color: '#fff' }}
           onClick={submitTx}
         >
           Submit
+        </button>
+      ) : isInstalled() && !address ? (
+        <button
+          className="header-button function"
+          type="button"
+          style={{ marginBottom: '1rem', color: '#fff' }}
+        >
+          No Wallet Connected
         </button>
       ) : (
         <button
           className="header-button function"
           type="button"
-          style={{ marginBottom: "1rem", color: "#fff" }}
+          style={{ marginBottom: '1rem', color: '#fff' }}
           onClick={() => window.open(METAMASK_DOWNLOAD_LINK)}
         >
           Install Metamask
         </button>
       )}
     </>
-  );
-};
+  )
+}
 
-export default FunctionButton;
+export default FunctionButton
